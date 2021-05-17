0b5d3941917be987fd94a16d042920561a55aa5c
EricJin101 <foreveryouth93@163.com>
2021-05-17 17:23:21 +0800

    chore: ~

--- c_plus/osqp-eigen/src/main.cc
+++ c_plus/osqp-eigen/src/main.cc
@@ -4,14 +4,97 @@
 int testQP();
 int testQP_2();
 int eric_simple_test();
+int testQP233();
 using namespace Eigen;
 
 int main() {
-  testQP();
-  //  testQP_2();
-  //  testQP_3();
+  testQP233();
+//    testQP_2();
+//    testQP_3();
 
   return 0;
+}
+
+int testQP233() {
+  // allocate QP problem matrices and vectors
+  Eigen::SparseMatrix<double> hessian;       // P or H
+  Eigen::VectorXd gradient;                  // f or q
+  Eigen::SparseMatrix<double> linearMatrix;  // A
+  Eigen::VectorXd lowerBound;                // l
+  Eigen::VectorXd upperBound;                // u
+
+  // 具有线性约束和边界的二次最小化
+  hessian.resize(2, 2);
+  hessian.insert(0, 0) = 4;
+  hessian.insert(1, 0) = -2;
+  hessian.insert(0, 1) = -2;
+  hessian.insert(1, 1) = 4;
+
+  std::cout << "hessian:" << std::endl << hessian << std::endl;
+
+  gradient.resize(2);
+  gradient << 6, 0;
+
+  std::cout << "gradient:" << std::endl << gradient << std::endl;
+
+  linearMatrix.resize(4, 2);
+  linearMatrix.insert(0, 0) = 1;
+  linearMatrix.insert(1, 0) = 1;
+  linearMatrix.insert(2, 0) = 0;
+  linearMatrix.insert(3, 0) = 1;
+
+  linearMatrix.insert(0, 1) = 1;
+  linearMatrix.insert(1, 1) = 0;
+  linearMatrix.insert(2, 1) = 1;
+  linearMatrix.insert(3, 1) = 1;
+
+  std::cout << "linearMatrix:" << std::endl << linearMatrix << std::endl;
+
+  lowerBound.resize(4);
+  lowerBound << 3, 0, 0, 2;
+  std::cout << "lowerBound:" << std::endl << lowerBound << std::endl;
+  upperBound.resize(4);
+  upperBound << 3, OsqpEigen::INFTY, OsqpEigen::INFTY, OsqpEigen::INFTY;
+  //  upperBound << 0, 0, 0, 0.5;
+  std::cout << "upperBound:" << std::endl << upperBound << std::endl;
+
+  //  int NumberOfVariables = 3; //A矩阵的列数
+  int NumberOfVariables = hessian.rows();         // A矩阵的列数
+  int NumberOfConstraints = linearMatrix.rows();  // A矩阵的行数
+
+  // instantiate the solver
+  OsqpEigen::Solver solver;
+
+  // settings
+  // solver.settings()->setVerbosity(false);
+  solver.settings()->setWarmStart(true);
+
+  // set the initial data of the QP solver
+  //矩阵A为m*n矩阵
+  solver.data()->setNumberOfVariables(
+      NumberOfVariables);  //设置A矩阵的列数，即n
+  solver.data()->setNumberOfConstraints(
+      NumberOfConstraints);  //设置A矩阵的行数，即m
+  if (!solver.data()->setHessianMatrix(hessian)) return 1;  //设置P矩阵
+  if (!solver.data()->setGradient(gradient))
+    return 1;  //设置q or f矩阵。当没有时设置为全0向量
+  if (!solver.data()->setLinearConstraintsMatrix(linearMatrix))
+    return 1;  //设置线性约束的A矩阵
+  if (!solver.data()->setLowerBound(lowerBound)) return 1;  //设置下边界
+  if (!solver.data()->setUpperBound(upperBound)) return 1;  //设置上边界
+
+  // instantiate the solver
+  if (!solver.initSolver()) return 1;
+
+  Eigen::VectorXd QPSolution;
+
+  // solve the QP problem
+  if (!solver.solve()) return 1;
+
+  // get the controller input
+  QPSolution = solver.getSolution();
+
+  std::cout << "QPSolution:" << std::endl << QPSolution << std::endl;
 }
 
 int testQP() {
@@ -173,100 +256,100 @@ int testQP_3() {
   std::cout << "QPSolution:" << std::endl << QPSolution << std::endl;
 }
 
-int testQP_2() {
-  /**
-   * f(x) = 0.5 x1^2 + x2^2 -x1 x2 - 2x1 -6x2
-   * s.t.
-   *        x1 + x2  <= 2
-   *       -x1 + 2x2 <= 2
-   *       2x1 + x2  <= 3
-   *  H = |  1  -1  |      f = | -2 |       x = |  x1  |
-   *      | -1   2  |          | -6 |           |  x2  |
-   *
-   * A = |   1  1  |       b = |  2  |
-   *     |  -1  2  |           |  2  |
-   *     |   2  1  |           |  3  |
-   * min(  0.5 X^T H X + f^T X  )
-   *  A X <= b
-   * */
-  // allocate QP problem matrices and vectors
-  Eigen::SparseMatrix<double> hessian;       // P or H
-  Eigen::VectorXd gradient;                  // f or q
-  Eigen::SparseMatrix<double> linearMatrix;  // A
-  Eigen::VectorXd lowerBound;                // l
-  Eigen::VectorXd upperBound;                // u
-
-  // 具有线性约束和边界的二次最小化
-  hessian.resize(2, 2);
-  hessian.insert(0, 0) = 1;
-  hessian.insert(0, 1) = -1;
-  hessian.insert(1, 0) = -1;
-  hessian.insert(1, 1) = 2;
-
-  std::cout << "hessian:" << std::endl << hessian << std::endl;
-
-  gradient.resize(2);
-  gradient << -2, -6;
-
-  std::cout << "gradient:" << std::endl << gradient << std::endl;
-
-  linearMatrix.resize(3, 2);
-  linearMatrix.insert(0, 0) = 1;
-  linearMatrix.insert(1, 0) = -1;
-  linearMatrix.insert(2, 0) = 2;
-
-  linearMatrix.insert(0, 1) = 1;
-  linearMatrix.insert(1, 1) = 2;
-  linearMatrix.insert(2, 1) = 1;
-
-  std::cout << "linearMatrix:" << std::endl << linearMatrix << std::endl;
-
-  lowerBound.resize(3);
-  lowerBound << -OsqpEigen::INFTY, -OsqpEigen::INFTY, -OsqpEigen::INFTY;
-  //  std::cout << "lowerBound:" << std::endl << lowerBound << std::endl;
-
-  upperBound.resize(3);
-  upperBound << 2, 2, 3;
-  //  upperBound << 0, 0, 0, 0.5;
-  std::cout << "upperBound:" << std::endl << upperBound << std::endl;
-
-  //  int NumberOfVariables = 3; //A矩阵的列数
-  int NumberOfVariables = linearMatrix.cols();    // A矩阵的列数
-  int NumberOfConstraints = linearMatrix.rows();  // A矩阵的行数
-  //  int NumberOfVariables = 2;  // A矩阵的列数
-  //  int NumberOfConstraints = 3;  // A矩阵的行数
-
-  // instantiate the solver
-  OsqpEigen::Solver solver;
-
-  // settings
-  // solver.settings()->setVerbosity(false);
-  solver.settings()->setWarmStart(true);
-
-  // set the initial data of the QP solver
-  //矩阵A为m*n矩阵
-  solver.data()->setNumberOfVariables(
-      NumberOfVariables);  //设置A矩阵的列数，即n
-  solver.data()->setNumberOfConstraints(
-      NumberOfConstraints);  //设置A矩阵的行数，即m
-  if (!solver.data()->setHessianMatrix(hessian)) return 1;  //设置P矩阵
-  if (!solver.data()->setGradient(gradient))
-    return 1;  //设置q or f矩阵。当没有时设置为全0向量
-  if (!solver.data()->setLinearConstraintsMatrix(linearMatrix))
-    return 1;  //设置线性约束的A矩阵
-  if (!solver.data()->setLowerBound(lowerBound)) return 1;  //设置下边界
-  if (!solver.data()->setUpperBound(upperBound)) return 1;  //设置上边界
-
-  // instantiate the solver
-  if (!solver.initSolver()) return 1;
-
-  Eigen::VectorXd QPSolution;
-
-  // solve the QP problem
-  if (!solver.solve()) return 1;
-
-  // get the controller input
-  QPSolution = solver.getSolution();
-
-  std::cout << "QPSolution:" << std::endl << QPSolution << std::endl;
-}
+//int testQP_2() {
+//  /**
+//   * f(x) = 0.5 x1^2 + x2^2 -x1 x2 - 2x1 -6x2
+//   * s.t.
+//   *        x1 + x2  <= 2
+//   *       -x1 + 2x2 <= 2
+//   *       2x1 + x2  <= 3
+//   *  H = |  1  -1  |      f = | -2 |       x = |  x1  |
+//   *      | -1   2  |          | -6 |           |  x2  |
+//   *
+//   * A = |   1  1  |       b = |  2  |
+//   *     |  -1  2  |           |  2  |
+//   *     |   2  1  |           |  3  |
+//   * min(  0.5 X^T H X + f^T X  )
+//   *  A X <= b
+//   * */
+//  // allocate QP problem matrices and vectors
+//  Eigen::SparseMatrix<double> hessian;       // P or H
+//  Eigen::VectorXd gradient;                  // f or q
+//  Eigen::SparseMatrix<double> linearMatrix;  // A
+//  Eigen::VectorXd lowerBound;                // l
+//  Eigen::VectorXd upperBound;                // u
+//
+//  // 具有线性约束和边界的二次最小化
+//  hessian.resize(2, 2);
+//  hessian.insert(0, 0) = 1;
+//  hessian.insert(0, 1) = -1;
+//  hessian.insert(1, 0) = -1;
+//  hessian.insert(1, 1) = 2;
+//
+//  std::cout << "hessian:" << std::endl << hessian << std::endl;
+//
+//  gradient.resize(2);
+//  gradient << -2, -6;
+//
+//  std::cout << "gradient:" << std::endl << gradient << std::endl;
+//
+//  linearMatrix.resize(3, 2);
+//  linearMatrix.insert(0, 0) = 1;
+//  linearMatrix.insert(1, 0) = -1;
+//  linearMatrix.insert(2, 0) = 2;
+//
+//  linearMatrix.insert(0, 1) = 1;
+//  linearMatrix.insert(1, 1) = 2;
+//  linearMatrix.insert(2, 1) = 1;
+//
+//  std::cout << "linearMatrix:" << std::endl << linearMatrix << std::endl;
+//
+//  lowerBound.resize(3);
+//  lowerBound << -OsqpEigen::INFTY, -OsqpEigen::INFTY, -OsqpEigen::INFTY;
+//  //  std::cout << "lowerBound:" << std::endl << lowerBound << std::endl;
+//
+//  upperBound.resize(3);
+//  upperBound << 2, 2, 3;
+//  //  upperBound << 0, 0, 0, 0.5;
+//  std::cout << "upperBound:" << std::endl << upperBound << std::endl;
+//
+//  //  int NumberOfVariables = 3; //A矩阵的列数
+//  int NumberOfVariables = linearMatrix.cols();    // A矩阵的列数
+//  int NumberOfConstraints = linearMatrix.rows();  // A矩阵的行数
+//  //  int NumberOfVariables = 2;  // A矩阵的列数
+//  //  int NumberOfConstraints = 3;  // A矩阵的行数
+//
+//  // instantiate the solver
+//  OsqpEigen::Solver solver;
+//
+//  // settings
+//  // solver.settings()->setVerbosity(false);
+//  solver.settings()->setWarmStart(true);
+//
+//  // set the initial data of the QP solver
+//  //矩阵A为m*n矩阵
+//  solver.data()->setNumberOfVariables(
+//      NumberOfVariables);  //设置A矩阵的列数，即n
+//  solver.data()->setNumberOfConstraints(
+//      NumberOfConstraints);  //设置A矩阵的行数，即m
+//  if (!solver.data()->setHessianMatrix(hessian)) return 1;  //设置P矩阵
+//  if (!solver.data()->setGradient(gradient))
+//    return 1;  //设置q or f矩阵。当没有时设置为全0向量
+//  if (!solver.data()->setLinearConstraintsMatrix(linearMatrix))
+//    return 1;  //设置线性约束的A矩阵
+//  if (!solver.data()->setLowerBound(lowerBound)) return 1;  //设置下边界
+//  if (!solver.data()->setUpperBound(upperBound)) return 1;  //设置上边界
+//
+//  // instantiate the solver
+//  if (!solver.initSolver()) return 1;
+//
+//  Eigen::VectorXd QPSolution;
+//
+//  // solve the QP problem
+//  if (!solver.solve()) return 1;
+//
+//  // get the controller input
+//  QPSolution = solver.getSolution();
+//
+//  std::cout << "QPSolution:" << std::endl << QPSolution << std::endl;
+//}
